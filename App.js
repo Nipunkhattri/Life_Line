28956b40d6adf5180c6455350626b7a835a6c599
nipunkhattri321 <nipunkhattri321@gmail.com>
2022-12-18 12:53:07 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { View } from 'react-native'
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import First from './components/First';
+import Second from './components/Second';
+import Signin from './components/Signin';
 
-export default function App() {
+
+const App = () => {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='First'>
+          <Stack.Screen name='First' component={First}/>
+          <Stack.Screen name='Second' component={Second}/>
+          <Stack.Screen name='Signin' component={Signin}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
---
+++ components/Second.js
@@ -0,0 +1,126 @@
+import React from 'react'
+import { Text,View,StyleSheet,Button ,Image,borderWidth,Pressable, TouchableOpacity} from 'react-native'
+
+
+const AppButton = ({ onPress, title }) => (
+    <TouchableOpacity onPress={onPress} style={styles.appButtonContainer}>
+      <Text style={styles.appButtonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+const SkipButton = ({ onPress, title }) => (
+    <TouchableOpacity onPress={onPress} style={styles.skipButtonContainer}>
+      <Text style={styles.skipButtonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+
+const Second = ({navigation}) => {
+  return (
+    <>
+      <View style={styles.main}>
+        <Image style={styles.tinyLogo} source={require('../rafiki.png')}/>
+        <Text style={styles.text}>You can also become a donor</Text>
+        <Text style={styles.stext}>
+        Through this app you can also become a donor to help some needed persons
+        </Text>
+        <View style={styles.circlecontain}>
+            <View style={styles.CircleShape1} />
+            <View style={styles.CircleShape2} />
+        </View>
+        <AppButton title="Next" size="sm" backgroundColor="#007bff" onPress={()=>{navigation.navigate("Signin")}}/>
+        <SkipButton title="Skip" size="sm" backgroundColor="#007bff" />
+        </View>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+    circlecontain:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        // backgroundColor: 'grey',
+        // height: 600
+        flex:0.1,  
+        width:30
+    },
+    CircleShape1: {
+        width: 10,
+        height: 10,
+        borderRadius: 15 / 2,
+        backgroundColor: '#2AA05D',
+        marginBottom:20
+      },
+    CircleShape2: {
+        width: 10,
+        height: 10,
+        borderRadius: 15 / 2,
+        backgroundColor: '#D9D9D9',
+        marginBottom:20
+      },
+    main:{
+        width:"100%",
+        height:"100%",
+        display:"flex",
+        justifyContent:"center",
+        alignItems:"center",
+        flex:1
+    },
+    tinyLogo: {
+        width: 300,
+        height: 300,
+        borderWidth:3,
+        flex:0.4
+    },
+    text:{
+        fontSize:32,
+        flexWrap:"wrap",
+        lineHeight:43,
+        width:330,
+        weight:600,
+        height:144,
+        flex:0.18,
+        color: "#555B63",
+        marginTop:30,
+        textAlign:"center"
+    },
+    stext:{
+        fontSize:20,
+        color:"#A8ABB3",
+        flexWrap:"wrap",
+        width:327,
+        flex:0.16,
+        // borderWidth:5,
+        textAlign:"center"
+    },
+    appButtonContainer: {
+        elevation: 8,
+        width:250,
+        backgroundColor: "#2AA05D",
+        borderRadius: 20,
+        paddingVertical: 10,
+        paddingHorizontal: 12,
+      },
+      appButtonText: {
+        fontSize: 18,
+        color: "#fff",
+        fontWeight: "bold",
+        alignSelf: "center",
+        textTransform: "uppercase"
+      },
+      skipButtonContainer:{
+        width:250,
+        // borderWidth:1,
+        marginTop:10,
+        borderRadius: 10,
+        paddingVertical: 10,
+        paddingHorizontal: 12
+      },
+      skipButtonText: {
+        fontSize: 18,
+        
+        fontWeight: "bold",
+        alignSelf: "center",
+        textTransform: "uppercase"
+      }  
+})
+export default Second
